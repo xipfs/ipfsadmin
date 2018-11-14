e66e57369687d4e7da0fd36983c26e2619a49984
flexie@foxmail.com <flexie@foxmail.com>
2018-11-14 20:27:10 +0800

    修改配置

--- app/controllers/config.go
+++ app/controllers/config.go
@@ -12,9 +12,6 @@ package controllers
 */
 
 import (
-	"encoding/json"
-	"fmt"
-
 	"github.com/xipfs/ipfsadmin/app/service"
 )
 
@@ -22,46 +19,8 @@ type ConfigController struct {
 	BaseController
 }
 
-//DownloadConfig 下载配置
-type DownloadConfig struct {
-	DownloadHeader map[string]map[string]string
-}
-
-type ConfigJson struct {
-	Version         string
-	CfgUpdatePeriod string
-	DataReport      *DataReport
-	IpfsConfig      map[string]string
-	DownloadConfig  *DownloadConfig
-}
-
-type DataReport struct {
-	ReportUrls          []string
-	BatchReportNum      int
-	MaxCacheFileSize    int
-	DisableEventActions []string
-	DataGathers         []DataGathers
-}
-
-type DataGathers struct {
-	Key         string
-	EventAction string
-	Param       map[string]string
-}
-
 // 获取状态
 func (this *ConfigController) Get() {
-	var r ConfigJson
-	configs, err := service.ConfigService.GetAllConfig()
-	this.checkError(err)
-	fmt.Println(configs)
-	for _, v := range configs {
-		if v.Key == "version1" {
-			err := json.Unmarshal([]byte(v.Value), &r)
-			if err != nil {
-				fmt.Printf("err was %v", err)
-			}
-		}
-	}
-	this.jsonResult(r)
+	config, _ := service.ConfigService.GetConfig(1)
+	this.Ctx.WriteString(config.Value)
 }
