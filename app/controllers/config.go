962a32e70119575e5ce7b9be7371a5d3b7c78507
flexie@foxmail.com <flexie@foxmail.com>
2019-02-26 10:55:01 +0800

    modify version3

--- app/controllers/config.go
+++ app/controllers/config.go
@@ -45,38 +45,40 @@ func (this *ConfigController) Get() {
 	requestBody := this.GetRequestBody()
 	json.Unmarshal(requestBody, p)
 	config := &entity.Config{}
-
-	if strings.Contains(p.ExtParams, "android") {
-		if strings.Contains(p.DynamicParams, "network:") {
-			if strings.Contains(p.DynamicParams, "WIFI") {
-				config, _ = service.ConfigService.GetConfig(1)
+	if p.Version == "3.0" {
+		config, _ = service.ConfigService.GetConfig(3)
+	} else {
+		if strings.Contains(p.ExtParams, "android") {
+			if strings.Contains(p.DynamicParams, "network:") {
+				if strings.Contains(p.DynamicParams, "WIFI") {
+					config, _ = service.ConfigService.GetConfig(1)
+				} else {
+					config, _ = service.ConfigService.GetConfig(2)
+				}
 			} else {
-				config, _ = service.ConfigService.GetConfig(2)
+				if strings.Contains(p.ExtParams, "WIFI") {
+					config, _ = service.ConfigService.GetConfig(1)
+				} else {
+					config, _ = service.ConfigService.GetConfig(2)
+				}
 			}
 		} else {
-			if strings.Contains(p.ExtParams, "WIFI") {
-				config, _ = service.ConfigService.GetConfig(1)
-			} else {
-				config, _ = service.ConfigService.GetConfig(2)
-			}
+			config, _ = service.ConfigService.GetConfig(1)
 		}
-	} else {
-		config, _ = service.ConfigService.GetConfig(1)
 	}
 	time := time.Now().Format("2006-01-02 15:04:05")
 	msg := fmt.Sprintf("%s\u0003%s\u0003%s\u0003%s\u0001%s\u0001%s\u0001%s\u0001%s\u0001%s\u0001%s\u0001%s\u0001%s\u0001%s\u0001%s\u0001%d\u0002", time, addr, p.Version, "config_get", p.Version, p.Version, p.PeerId, p.Goarch, "", "", p.TimeStr, "", p.ExtParams, p.DynamicParams, config.Id)
 	if p.Version == "3.0" {
 		logs.Info(msg)
 		if flag {
-			SendToKafka(msg, "test")
+			SendToKafka(msg, "baize")
 		} else {
 			err := InitKafka()
 			if err != nil {
 
 			} else {
-				SendToKafka(msg, "test")
+				SendToKafka(msg, "baize")
 			}
-
 		}
 	}
 	this.Ctx.WriteString(config.Value)
